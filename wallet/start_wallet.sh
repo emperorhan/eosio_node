4d818eb21057f1da7aa6f3e12453077c6b59037f
Emperorhan <iny27@naver.com>
2018-12-04 23:30:42 +0900

    init

--- wallet/start_wallet.sh
+++ wallet/start_wallet.sh
@@ -4,5 +4,5 @@ WALLET_HOST="127.0.0.1"
 WALLET_POSRT="3000"
 
 
-/home/eos/ibctprivate/eosio/wallet/stop_wallet.sh
+/home/eos/ibctprivate/eosio_node/wallet/stop_wallet.sh
 $NODEOSBINDIR/keosd/keosd --config-dir $DATADIR --wallet-dir $DATADIR --http-server-address $WALLET_HOST:$WALLET_POSRT $@ & echo $! > $DATADIR/wallet.pid
